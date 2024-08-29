b9634357237b472768d581c8b82c2fc8a1400b98
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-29 17:13:24 +0700

    Configure Kinde authentication

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+	LoginLink,
+	RegisterLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
 
 import { NavbarLinks } from './NavbarLinks'
 import { MobileMenu } from './MobileMenu'
 import { Button } from '@/components/ui/button'
 
 export async function Navbar() {
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
 	return (
 		<nav className='relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7'>
 			<div className='md:col-span-3'>
@@ -18,8 +26,18 @@ export async function Navbar() {
 			<NavbarLinks />
 
 			<div className='flex items-center gap-x-2 ms-auto md:col-span-3'>
-				<Button>Login</Button>
-				<Button variant='secondary'>Register</Button>
+				{user ? (
+					<h1>User is authenticated</h1>
+				) : (
+					<div className='flex items-center gap-x-2'>
+						<Button asChild>
+							<LoginLink>Login</LoginLink>
+						</Button>
+						<Button asChild variant='secondary'>
+							<RegisterLink>Register</RegisterLink>
+						</Button>
+					</div>
+				)}
 			</div>
 
 			<div className='md:hidden'>
