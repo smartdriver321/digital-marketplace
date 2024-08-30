c91c6f9d9d88c048ea474795222d732759964cd6
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-30 19:20:49 +0700

    Create HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { ProductRow } from '@/components/shared/ProductRow'
+
 export default function HomePage() {
 	return (
-		<div>
-			<h1 className='bg-red-500'>Hello from HomePage</h1>
-		</div>
+		<section className='max-w-7xl mx-auto px-4 md:px-8 mb-24'>
+			<div className='max-w-3xl mx-auto text-2xl sm:text-5xl lg:text-6xl font-semibold text-center'>
+				<h1>Find the best Tailwind</h1>
+				<h1 className='text-primary'>Templates & Icons</h1>
+				<p className='lg:text-lg text-muted-foreground mx-auto mt-5 w-[90%] font-normal text-base'>
+					MarshalUI stands out as the premier marketplace for all things related
+					to tailwindcss, offering an unparalleled platform for both sellers and
+					buyers alike.
+				</p>
+			</div>
+			<ProductRow category='newest' />
+			<ProductRow category='templates' />
+			<ProductRow category='icons' />
+			<ProductRow category='uikits' />
+		</section>
 	)
 }
